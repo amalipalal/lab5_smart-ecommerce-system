ed5c711a07209889caf81e66d89052348ef06823
palalasare <pinopalal11@gmail.com>
2026-01-26 17:52:02 +0000

    feat: add dto classes
    - ProductRequestDto
    - ProductResponseDto

---
+++ src/main/java/com/example/ecommerce_system/dto/product/ProductRequestDto.java
@@ -0,0 +1,43 @@
+package com.example.ecommerce_system.dto.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class ProductRequestDto {
+    @NotBlank(
+            groups = CreateProductRequest.class,
+            message = "name cannot be empty"
+    )
+    private String name;
+
+    @NotBlank(
+            groups = CreateProductRequest.class,
+            message = "description cannot be empty"
+    )
+    private String description;
+
+    @PositiveOrZero(
+            groups = {CreateProductRequest.class, UpdateProductRequest.class},
+            message = "price cannot be less than 0"
+    )
+    Double price;
+
+    @PositiveOrZero(
+            groups = {CreateProductRequest.class, UpdateProductRequest.class},
+            message = "stock cannot be less than 0"
+    )
+    Integer stock;
+
+    @NotNull(
+            groups = {CreateProductRequest.class, UpdateProductRequest.class},
+            message = "categoryId cannot be empty"
+    )
+    UUID categoryId;
+}
