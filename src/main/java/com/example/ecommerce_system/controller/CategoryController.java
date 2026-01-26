a7e8ea8e09e4b97fc124ad04a6123ef3c4a7dcdc
palalasare <pinopalal11@gmail.com>
2026-01-26 10:25:13 +0000

    refactor: use SuccessResponseHandler in CategoryController

--- src/main/java/com/example/ecommerce_system/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce_system/controller/CategoryController.java
@@ -1,22 +1,21 @@
 package com.example.ecommerce_system.controller;
 
-import com.example.ecommerce_system.dto.CategoryResponseDto;
-import com.example.ecommerce_system.dto.SuccessResponseDto;
+import com.example.ecommerce_system.dto.*;
 import com.example.ecommerce_system.service.CategoryService;
+import com.example.ecommerce_system.util.SuccessResponseHandler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Validated
@@ -30,12 +29,12 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "All categories retrieved"),
     })
     @GetMapping
-    public SuccessResponseDto<List<CategoryResponseDto>> getCategories(
+    public SuccessResponseDto<List<CategoryResponseDto>> getAllCategories(
             @RequestParam @Min(1) @Max(100) int limit,
             @RequestParam @Min(0) int offset)
     {
         List<CategoryResponseDto> categories = categoryService.getAllCategories(limit, offset);
-        return new SuccessResponseDto<>(HttpStatus.OK, "Success", categories);
+        return SuccessResponseHandler.generateSuccessResponse(HttpStatus.OK, categories);
     }
 
     @Operation(summary = "Retrieve a single category by categoryId")
