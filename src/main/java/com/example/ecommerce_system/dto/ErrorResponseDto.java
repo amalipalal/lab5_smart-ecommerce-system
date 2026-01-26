6373668ec0a659afe96103e97482045fa46f5d6d
palalasare <pinopalal11@gmail.com>
2026-01-26 10:21:58 +0000

    refactor: update naming in ErrorResponseDto

--- src/main/java/com/example/ecommerce_system/dto/ErrorResponseDto.java
+++ src/main/java/com/example/ecommerce_system/dto/ErrorResponseDto.java
@@ -2,4 +2,4 @@ package com.example.ecommerce_system.dto;
 
 import org.springframework.http.HttpStatus;
 
-public record ErrorResponseDto<T> (HttpStatus status, String error, T data){}
+public record ErrorResponseDto<T> (HttpStatus status, String message, T error){}
