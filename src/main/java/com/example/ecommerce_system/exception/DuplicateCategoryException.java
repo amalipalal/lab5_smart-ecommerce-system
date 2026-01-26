6f40633e32730dc8e15cb69c3993316d918ca5aa
palalasare <pinopalal11@gmail.com>
2026-01-26 10:23:55 +0000

    feat: add CategoryService specific handlers
    - Response handler methods were added to CategoryResponseHandler

--- src/main/java/com/example/ecommerce_system/exception/DuplicateCategoryException.java
+++ src/main/java/com/example/ecommerce_system/exception/DuplicateCategoryException.java
@@ -2,6 +2,6 @@ package com.example.ecommerce_system.exception;
 
 public class DuplicateCategoryException extends RuntimeException {
     public DuplicateCategoryException(String name) {
-        super("Category with name '" + name + "' already exists");
+        super("Category with name '" + name.toLowerCase() + "' already exists");
     }
 }
