1b3161c0b6045af1b323cb57f7a2b32afbcda841
amalipalal <palal.asare@amalitech.com>
2026-02-06 17:13:29 +0000

    refactor: remove unused files

--- src/main/java/com/example/ecommerce_system/exception/review/OrderNotProcessedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce_system.exception.review;
-
-public class OrderNotProcessedException extends RuntimeException {
-    public OrderNotProcessedException(String orderId) {
-        super(String.format("Order %s has not been processed yet. Only processed orders can be reviewed.", orderId));
-    }
-}
