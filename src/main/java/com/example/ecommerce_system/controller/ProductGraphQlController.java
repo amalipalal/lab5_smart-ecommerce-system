e8fffeaf14857f71d2483ebca37d568ab6eed2c9
amalipalal <palal.asare@amalitech.com>
2026-02-06 10:54:03 +0000

    feat: add graphql request processor
    - add ProductGraphQlController
    - map getAllProductsWithReviews to orders-schema.graphqls

---
+++ src/main/java/com/example/ecommerce_system/controller/ProductGraphQlController.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce_system.controller;
+
+import com.example.ecommerce_system.dto.product.ProductWithReviewsDto;
+import com.example.ecommerce_system.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class ProductGraphQlController {
+
+    private final ProductService productService;
+
+    /**
+     * Retrieves all products with their reviews.
+     */
+    @QueryMapping
+    public List<ProductWithReviewsDto> getAllProductsWithReviews(
+            @Argument int limit,
+            @Argument int offset,
+            @Argument(name = "reviewLimit") int reviewLimit
+    ) {
+        return productService.getAllProductsWithReviews(limit, offset, reviewLimit);
+    }
+}
