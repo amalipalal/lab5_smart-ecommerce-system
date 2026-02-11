b0fc0ce51c6ab821b681dc65d98a45524b42d7eb
palalasare <pinopalal11@gmail.com>
2026-02-11 10:31:00 +0000

    feat: configure pathpatterns for interceptors

---
+++ src/main/java/com/example/ecommerce_system/config/InterceptorConfig.java
@@ -0,0 +1,35 @@
+package com.example.ecommerce_system.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@AllArgsConstructor
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+    private final AdminInterceptor adminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/v1/**")
+                .excludePathPatterns(
+                        "/api/v1/auth/**",
+                        "**/docs",
+                        "/api/swagger-ui/**",
+                        "**/v3/api-docs/**"
+                );
+
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/api/v1/**")
+                .excludePathPatterns(
+                        "/api/v1/auth/**",
+                        "**/docs",
+                        "/api/swagger-ui/**",
+                        "**/v3/api-docs/**"
+                );
+    }
+}
